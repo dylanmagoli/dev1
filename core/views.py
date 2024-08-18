f3c7b8879e4c670ae4293535ea6f2d9d24188f9c
dylanmagoli <152192240+dylanmagoli@users.noreply.github.com>
2024-08-18 14:51:15 -0700

    Add files via upload

---
+++ core/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+
+from item.models import Category, Item
+
+from .forms import SignupForm
+
+def index(request):
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all() 
+      
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items,
+    })
+
+def contact(request):
+    return render(request, 'core/contact.html')    
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login')
+    else:
+        form= SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query':query,
+        'categories': categories, 
+        'category_id': int(category_id)
+    })
